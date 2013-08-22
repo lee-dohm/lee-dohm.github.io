e5e2229a6eaeec2010b85fc43f1f0fa136d705d9
Lee Dohm <lee@liftedstudios.com>
2013-08-21 19:38:31 -0700

    Fix #3: Add copyright_range tag for better generation of copyright year range

---
+++ _plugins/copyright_range.rb
@@ -0,0 +1,29 @@
+#
+# Copyright (c) 2013 by Lifted Studios. All Rights Reserved.
+#
+
+module Jekyll
+  # A Liquid tag for displaying the range of copyright years from the earliest
+  # post to the current year.
+  class CopyrightRangeTag < Liquid::Tag
+    # Returns the text to replace the tag with in the final document.
+    #
+    # @param  context [Liquid::Context] Current Liquid template engine state.
+    # @return [String] Text to put in place of the tag.
+    def render(context)
+      base_year = calculate_base_year(context)
+      this_year = Time.now.year
+      base_year == this_year ? base_year : "#{base_year}-#{this_year}"
+    end
+
+    # Determines the year of the earliest post.
+    #
+    # @param context [Liquid::Context] Current Liquid template engine state.
+    # @return [Integer] Year of the earliest post.
+    def calculate_base_year(context)
+      context.registers[:site].posts.map { |post| post.date.year }.min
+    end
+  end
+end
+
+Liquid::Template.register_tag('copyright_range', Jekyll::CopyrightRangeTag)
