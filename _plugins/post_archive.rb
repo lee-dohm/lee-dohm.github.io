e78b65dc82f257d84a17bf2d93dada55145c0a92
Lee Dohm <lee@liftedstudios.com>
2013-10-06 09:56:41 -0700

    Fix archive generation code

---
+++ _plugins/post_archive.rb
@@ -0,0 +1,61 @@
+#
+# Copyright (c) 2013 by Lifted Studios. All Rights Reserved.
+#
+
+module Jekyll
+  class PostArchiveTag < Liquid::Tag
+    def render(context)
+      posts = context.registers[:site].posts
+
+      render_years(posts)
+    end
+
+    def render_years(posts)
+      lines = []
+      lines << "<!-- Rendering years -->"
+
+      years = posts.map { |post| post.date.year }.uniq.sort { |x,y| y <=> x }
+      years.each do |year|
+        lines << "<h1><p class=\"text-center\">#{year}</p></h1>"
+        lines << render_year(year, posts)
+      end
+
+      lines.flatten.join("\n")
+    end
+
+    def render_year(year, posts)
+      lines = []
+      lines << "<!-- Rendering year #{year} -->"
+      lines << '<!-- Rendering months -->'
+
+      months = posts.select { |post| post.date.year == year }.
+                     sort { |x,y| y.date.month <=> x.date.month }.
+                     map { |post| [post.date.month, post.date.strftime('%B')] }.uniq
+      months.each do |month|
+        lines << "<h2>#{month.last}</h2>"
+        lines << render_month(year, month.first, posts)
+      end
+
+      lines
+    end
+
+    def render_month(year, month, posts)
+      lines = []
+      lines << "<!-- Rendering month #{month} #{year} -->"
+      lines << '<!-- Rendering posts -->'
+
+      ps = posts.select { |post| post.date.year == year && post.date.month == month }.
+                 sort { |x,y| y.date <=> x.date }
+
+      lines << '<ul>'
+      ps.each do |post|
+        lines << "<li><a href=\"#{post.url}\">#{post.title}</a></li>"
+      end
+      lines << '</ul>'
+
+      lines
+    end
+  end
+end
+
+Liquid::Template.register_tag('post_archive', Jekyll::PostArchiveTag)
